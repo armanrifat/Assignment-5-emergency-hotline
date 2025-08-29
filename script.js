4c5144252ede4034afe2e2906d1fc2f0676bc8b2
mac mini m1 <macminim1@macs-Mac-mini.local>
2025-08-29 07:02:30 +0600

    add history container

--- script.js
+++ script.js
@@ -4,3 +4,7 @@
 const heartCount = document.getElementById("heart-img");
 const coinCount = document.getElementById("coin-img");
 const copyCount = document.getElementById("copy-count");
+
+// history container
+const historyContainer = document.getElementById("history-container");
+const clearBtn = document.getElementById("clear-btn");
