2a8f137f7d2da99e0df223ebd5343146ee90ceee
mac mini m1 <macminim1@macs-Mac-mini.local>
2025-08-29 07:03:34 +0600

    addHeart icon click

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ const copyCount = document.getElementById("copy-count");
 // history container
 const historyContainer = document.getElementById("history-container");
 const clearBtn = document.getElementById("clear-btn");
+
+// ❤️ Heart icon click
+document.querySelectorAll(".card-heart").forEach((heart) => {
+  heart.addEventListener("click", () => {
+    let current = parseInt(heartCount.textContent);
+    heartCount.textContent = current + 1;
+  });
+});
